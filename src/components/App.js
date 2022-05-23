dc8c7c3a837def87c2fcd95b69c6181b7a304675
Teresa Lew <teresalew92@gmail.com>
2022-05-22 18:02:50 -0700

    Configure ESLint, Prettier, and set up dependencies

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));

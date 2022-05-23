2379c719b5846270f858390ece2b40b273cd696a
Teresa Lew <teresalew92@gmail.com>
2022-05-22 20:19:58 -0700

    Render out pet listing fetched from API

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import ReactDOM from "react-dom";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me</h1>
+      <SearchParams />
     </div>
   );
 };
